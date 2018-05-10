489894e416bb152b844128a7c13971a8de34aada
Marco Nie <nie11kun@outlook.com>
2018-05-10 15:24:14 +0800

    init

---
+++ sololearn/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << "this is marco!" << endl;
+    cout << "this is my first c++ project.\n\n";
+    cout << "new line" << endl;
+    int myVarible = 10;
+    cout << myVarible << endl;
+
+    int var1 = 2;
+    int var2 = 3;
+    int sum1 = var1 + var2;
+
+    cout << sum1;
+    return 0;
+}
+//this is comment
+
+/*
+multi lines
+*/
+
+
